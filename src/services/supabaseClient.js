36ecd9990c4c98b59748f7b3df6f05a45d5d7d76
Deniihsilvaa <deni1313.dd@gmail.com>
2024-11-19 15:05:22 -0300

    Update configuracao netlify 3

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://xwhvvbueavhkbuaoujcb.supabase.co'; // URL do seu Supabase
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh3aHZ2YnVlYXZoa2J1YW91amNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE4OTQ4MjQsImV4cCI6MjA0NzQ3MDgyNH0.6jnaT_cluYkYyOUQQ2T55FhHPcMmr_JUUuinEI6QJAE'; // Chave pública de autenticação
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
